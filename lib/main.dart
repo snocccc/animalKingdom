a15eaf52d05fd0424ae73bee83e5aa5198fa72e4
snocccc <gaa.concon25@gmail.com>
2024-06-20 12:09:44 +0800

    salksla

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,72 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text('Animal Kingdom App'),
-        ),
-        backgroundColor: Colors.lightBlue[700],
-      ),
-      body: Center(
-        child: Text(
-          'Welcome to the Jungle',
-          style: TextStyle(
-            fontSize: 24.0,
-            fontWeight: FontWeight.bold,
-            color: Colors.grey[850],
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('User Profile'),
+      backgroundColor: Colors.deepPurple[700],
+      centerTitle: true,
+    ),
+    body: Padding(
+      padding: EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'NAME',
+             style: TextStyle(
+               fontSize: 18.0,
+               color: Colors.deepPurple[500],
+               letterSpacing: 2.0,
+             )
           ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){},
-        child: Text('Click Me'),
+          Text(
+            'Cons Henren Gaa',
+             style: TextStyle(
+               fontWeight: FontWeight.bold,
+               color: Colors.deepPurple[700],
+               fontSize: 25.0,
+             ),
+          ),
+          SizedBox(height: 30.0,),
+          Text(
+            'YEAR',
+            style: TextStyle(
+              fontSize: 16.0,
+              color: Colors.deepPurple[500],
+              letterSpacing: 2.0,
+            ),
+          ),
+          Text(
+            '4TH',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.deepPurple[700],
+              fontSize: 25.0,
+            ),
+          ),
+          SizedBox(height: 30.0,),
+          Text(
+            'Email',
+            style: TextStyle(
+              fontSize: 16.0,
+              color: Colors.deepPurple[500],
+              letterSpacing: 2.0,
+            ),
+          ),
+          Text(
+            'Gaa.concon25@gmail.com',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.deepPurple[700],
+              fontSize: 25.0,
+            ),
+          ),
+        ],
       ),
     ),
-  ));
-}
+  ),
+));
 
 
 
